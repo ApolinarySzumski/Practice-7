05c9ffb75bd68a2e6807df56270d2b81f40e424e
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-09-18 13:50:59 +0200

    2/2 tasks done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map((item) => {
+    return `<li>
+    <a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt=${item.description} />
+</a>
+</li>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
